ee1917086b0ba526920507ef2e8c2c7a09f769a3
Félix Piédallu <felix@piedallu.me>
2015-04-13 00:13:33 +0200

    added actionneur.h

--- Robot 2A/actionneurs.c
+++ Robot 2A/actionneurs.c
@@ -3,50 +3,39 @@
 // Empileur
 // // Porte
 int empileur_porte_ouvre() {
-
 }
 int empileur_porte_ferme() {
-
 }
 // // Monte-pieds
+int empileur_monteur_descend(){
+}
+int empileur_monteur_monte_un_pied(){
+}
+int empileur_monteur_prend_une_ampoule(){
+}
 
 
 // Bras latéraux
 int bras_droit_ferme() {
-    
 }
-
 int bras_droit_position_clap() {
-    
 }
-
 int bras_droit_coince_verre() {
-    
 }
-
 int bras_droit_libere_verre() {
-    
 }
 
 
 int bras_gauche_ferme() {
-    
 }
-
 int bras_gauche_position_clap() {
-    
 }
-
 int bras_gauche_coince_verre() {
-    
 }
-
 int bras_gauche_libere_verre() {
-    
 }
 
 
 // Attrape-popcorns
 int attrape_popcorns_tourne(int position) {
-    
 }
