0edf45d8441828389cad510de5cea7c4e57fb530
Félix Piédallu <felix@piedallu.me>
2015-04-13 14:36:38 +0200

    added return statements

--- Robot 2A/actionneurs.c
+++ Robot 2A/actionneurs.c
@@ -3,39 +3,53 @@
 // Empileur
 // // Porte
 int empileur_porte_ouvre() {
+    return 1;
 }
 int empileur_porte_ferme() {
+    return 1;
 }
 // // Monte-pieds
 int empileur_monteur_descend() {
+    return 1;
 }
 int empileur_monteur_monte_un_pied() {
+    return 1;
 }
 int empileur_monteur_prend_une_ampoule() {
+    return 1;
 }
 
 
 // Bras latéraux
 int bras_droit_ferme() {
+    return 1;
 }
 int bras_droit_position_clap() {
+    return 1;
 }
 int bras_droit_coince_verre() {
+    return 1;
 }
 int bras_droit_libere_verre() {
+    return 1;
 }
 
 
 int bras_gauche_ferme() {
+    return 1;
 }
 int bras_gauche_position_clap() {
+    return 1;
 }
 int bras_gauche_coince_verre() {
+    return 1;
 }
 int bras_gauche_libere_verre() {
+    return 1;
 }
 
 
 // Attrape-popcorns
 int attrape_popcorns_tourne(int position) {
+    return 1;
 }
