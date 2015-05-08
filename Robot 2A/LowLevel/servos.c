a9e4dee7276b115682b6daa4535640bb9e670cfa
larchuto <johan.lafon@phelma.grenoble-inp.fr>
2015-05-08 20:26:45 +0200

    réanrange les inits + correction d'un include

--- Robot 2A/LowLevel/servos.c
+++ Robot 2A/LowLevel/servos.c
@@ -1,38 +1,56 @@
-#include ../mapping/PWM.h
+#include "../mapping/PWM.h"
+
+void init_bras_gauche();
+void init_bras_droit();
+void init_porte_empileur();
+void init_attrape_popcorns();
 
 Servo_t servo_bras_gauche;
 Servo_t servo_bras_droit;
 Servo_t servo_porte_empileur;
 Servo_t servo_attrape_popcorns;
 
-void init_bras_gauche() {
-    //TODO : Donner les bonnes valeurs
-    init_servo(&servo_bras_gauche, PWM2_PORT, PWM2_PIN);
+
+void init_all_servo() {
+    init_bras_gauche();
+    init_bras_droit();
+    init_porte_empileur();
+    init_attrape_popcorns();
 }
+
+
 int bras_gauche_set_angle(int angle) {
     set_position_servo(angle, &servo_bras_gauche);
 }
 
-void init_bras_droit() {
-    //TODO : Donner les bonnes valeurs
-    init_servo(&servo_bras_droit, PWM1_PORT, PWM1_PIN);
-}
+
 int bras_droit_set_angle(int angle) {
     set_position_servo(angle, &servo_bras_droit);
 }
 
-void init_porte_empileur() {
-    //TODO : Donner les bonnes valeurs
-    init_servo(&servo_porte_empileur, PWM4_PORT, PWM4_PIN);
-}
+
 int porte_empileur_set_angle(int angle) {
     set_position_servo(angle, &servo_porte_empileur);
 }
 
-void init_attrape_popcorns() {
-    //TODO : Donner les bonnes valeurs
-    init_servo(&servo_attrape_popcorns, PWM3_PORT, PWM3_PIN);
-}
 int attrape_popcorns_set_angle(int angle) {
     set_position_servo(angle, &servo_attrape_popcorns);
+}
+
+
+//init
+void init_bras_gauche() {
+    init_servo(&servo_bras_gauche, PWM2_PORT, PWM2_PIN);
+}
+
+void init_bras_droit() {
+    init_servo(&servo_bras_droit, PWM1_PORT, PWM1_PIN);
+}
+
+void init_porte_empileur() {
+    init_servo(&servo_porte_empileur, PWM4_PORT, PWM4_PIN);
+}
+
+void init_attrape_popcorns() {
+    init_servo(&servo_attrape_popcorns, PWM3_PORT, PWM3_PIN);
 }
--- Robot 2A/LowLevel/servos.h
+++ Robot 2A/LowLevel/servos.h
@@ -1,16 +1,12 @@
 #ifndef SERVOS_H
 #define SERVOS_H
 
-void init_bras_gauche();
+void init_all_servo();
+
+
 int bras_gauche_set_angle(int angle);
-
-void init_bras_droit();
 int bras_droit_set_angle(int angle);
-
-void init_porte_empileur();
 int porte_empileur_set_angle(int angle);
-
-void init_attrape_popcorns();
 int attrape_popcorns_set_angle(int angle);
 
 #endif
