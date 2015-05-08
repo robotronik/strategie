401309ea5ea0bac921d7756e6533bd0479ef1ea3
larchuto <johan.lafon@phelma.grenoble-inp.fr>
2015-05-08 23:55:23 +0200

    ajout reglages

---
+++ Robot 2A/Calibration/reglages_ascenseur.h
@@ -0,0 +1,13 @@
+#ifndef REGLAGES_ASCENSEUR_H
+#define REGLAGES_ASCENSEUR_H
+
+//pont en H
+#define PWM_MOTEUR 1
+#define DIR_MONTEE 1
+#define DIR_DESCENTE 0
+
+//temps pour atteindre les différentes positions
+#define TPS_MONTEE_PIED 0
+#define TPS_MONTEE_BALLE 0
+
+#endif
