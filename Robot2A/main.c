805a7e53e4d5d4631c6d3fe9d1cf39d829577dab
larchuto <johan.lafon@phelma.grenoble-inp.fr>
2015-05-10 16:42:01 +0200

    ajoute des tests pour les différents actionneurs

--- Robot2A/main.c
+++ Robot2A/main.c
@@ -5,12 +5,76 @@
 #include "../../cartographie/cartographie.h"
 #include "../../common_code/uart_emission.h"
 #include "../../common_code/debug.h"
+#include "actionneurs.h"
+#include "utilities.h"
+#include "LowLevel/empileur.h"
+#include "../mapping/PWM_pinout.h"
+
+//test pwm
+#include "../mapping/IO_pinout.h"
+#include "../mapping/PWM_pinout.h"
+#include "../../stm32f407/headers/GPIO.h"
+#include "../../stm32f407/headers/PWM.h"
 #include "utilities.h"
 
+//test servo
+//#include "../../mapping/PWM_pinout.h"
+#include "../../stm32f407/headers/servo.h"
+
+s_PWM moteur_empileur;
+Servo_t servo_porte_empileur;
+
 int mainStrategie() {
-    init();
-    init_GPIO_LED();
+    init_actionneurs();
     test_led();
+
+    //test du bras droit
+    /*while(1) {
+        bras_droit_position_clap();
+        Delay(1000);
+        bras_droit_repos();
+        Delay(1000);
+    }*/
+    /*while(1) {
+        bras_droit_position_prise_gobelet();
+        Delay(1000);
+        bras_droit_coince_gobelet();
+        Delay(1000);
+    }*/
+
+    //test du bras gauche
+    /*while(1) {
+        bras_gauche_position_clap();
+        Delay(1000);
+        bras_gauche_repos();
+        Delay(1000);
+    }*/
+    /*while(1) {
+        bras_gauche_position_prise_gobelet();
+        Delay(1000);
+        bras_gauche_coince_gobelet();
+        Delay(1000);
+    }*/
+
+    //test du moulin
+    /*while(1)
+    {
+        prend_pop_corn();
+        Delay(1000);
+        libere_pop_corn();
+        Delay(1000);
+    }*/
+
+    //test de l'empileur
+    while(1)
+    {
+        if (rupteur_pied_empileur_is_pushed()) {
+            descend_ascenseur();
+            Delay(100);
+            monte_pied();
+        }
+    }
+
     /*x_futur = x_actuel;
     y_futur = y_actuel;
     pathfinding_init();
