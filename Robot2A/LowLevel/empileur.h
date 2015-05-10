e7a87d47db02a9f5b153f004effa462beeb94654
larchuto <johan.lafon@phelma.grenoble-inp.fr>
2015-05-10 16:35:58 +0200

    fini la partie empileur

--- Robot2A/LowLevel/empileur.h
+++ Robot2A/LowLevel/empileur.h
@@ -5,6 +5,7 @@
 
 void init_empileur();
 
+void stop_moteur_empileur();
 void set_PWM_moteur_empileur(float pwm, int direction);
 int rupteur_pied_empileur_is_pushed();
 int rupteur_moteur_empileur_is_pushed();
