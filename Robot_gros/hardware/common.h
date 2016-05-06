59d64445f5ac5df676eb954072c3c9fdd9afecc4
Félix Piédallu <salamandar@excalibur.home>
2016-05-06 18:39:53 +0200

    LEDs de débug + tirette

--- Robot_gros/hardware/common.h
+++ Robot_gros/hardware/common.h
@@ -17,3 +17,15 @@ void arret_capteurs();
 void led_verte_on();
 void led_verte_off();
 void set_all_led();
+
+void init_leds_debug();
+void set_leds();
+void clear_leds();
+void set_ledBleue();
+void set_ledOrange();
+void set_ledVerte();
+void set_ledRouge();
+void clear_ledBleue();
+void clear_ledOrange();
+void clear_ledVerte();
+void clear_ledRouge();
--- Robot_gros/hardware/libopencm3/common.c
+++ Robot_gros/hardware/libopencm3/common.c
@@ -8,12 +8,13 @@ void init_hardware() {
     clock_setup();
 
     gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_PULLDOWN, GPIO0); // Bouton User
-    gpio_mode_setup(GPIOE, GPIO_MODE_INPUT, GPIO_PUPD_PULLUP, GPIO4); // Tirette
+    gpio_mode_setup(GPIOE, GPIO_MODE_INPUT, GPIO_PUPD_PULLUP, GPIO2); // Tirette
     gpio_mode_setup(GPIOE, GPIO_MODE_INPUT, GPIO_PUPD_PULLUP, GPIO9); // Choix de couleur
     gpio_mode_setup(GPIOE, GPIO_MODE_INPUT, GPIO_PUPD_NONE,   GPIO10); //pin de retour de l'asservissement
 
     init_alarms_and_delay();
     init_leds();
+    init_leds_debug();
     //init_RTC();
 
     //init_pwm_timer(TIM4, 1000);
@@ -31,7 +32,7 @@ int read_user_button() {
 }
 
 int read_tirette() {
-    return gpio_get(GPIOE, GPIO4);
+    return gpio_get(GPIOE, GPIO2);
 }
 
 int read_color_button() {
@@ -52,4 +53,50 @@ void active_capteurUS_2() {
 
 void arret_capteurs() {
   //  HAL_NVIC_DisableIRQ(TIM5_IRQn);
+}
+
+void init_leds_debug(){
+  rcc_periph_clock_enable(RCC_GPIOE);
+  
+  gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO3 | GPIO4 | GPIO5 | GPIO6);
+}
+
+void set_leds(){
+  gpio_set(GPIOE, GPIO3 | GPIO4 | GPIO5 | GPIO6);
+}
+
+void clear_leds(){
+  gpio_clear(GPIOE, GPIO3 | GPIO4 | GPIO5 | GPIO6);
+}
+
+void set_ledBleue(){
+  gpio_set(GPIOE, GPIO3);
+}
+
+void set_ledOrange(){
+  gpio_set(GPIOE, GPIO5);
+}
+
+void set_ledVerte(){
+  gpio_set(GPIOE, GPIO6);
+}
+
+void set_ledRouge(){
+  gpio_set(GPIOE, GPIO4);
+}
+
+void clear_ledBleue(){
+  gpio_clear(GPIOE, GPIO3);
+}
+
+void clear_ledOrange(){
+  gpio_clear(GPIOE, GPIO5);
+}
+
+void clear_ledVerte(){
+  gpio_clear(GPIOE, GPIO6);
+}
+
+void clear_ledRouge(){
+  gpio_clear(GPIOE, GPIO4);
 }
